3f3b37c20553d80c0e762a692f58a6ba77324808
Ben Hall <149907310+Green13248@users.noreply.github.com>
2024-11-17 15:37:56 -0500

    Update Cube.py

--- Cube.py
+++ Cube.py
@@ -1,31 +1,20 @@
 import pygame
-import numpy
-
 bgcolor = (0, 0, 0) 
-  
 screen = pygame.display.set_mode((500, 500)) 
-
 pygame.display.set_caption('3D cube') 
-
 screen.fill(bgcolor) 
-
-pygame.display.flip()
-
-FOV = 70
 cube = []
-
 def render(x, y, z):
-    projectedx = x * FOV/FOV + z
-    projectedy = y * FOV/FOV + z
-    cube.append(x)
-    cube.append(y)
-    cube.append(z)
+    FL = 75
+    Px = (500/2) + (x*FL) / (FL+z)
+    Py = (500/2) + (y*FL) / (FL+z)
+    cube.append(Px)
+    cube.append(Py)
     print(cube)
     pygame.draw.line(screen, (255, 255, 255), 
-                 [projectedx, projectedy], 
-                 [projectedx + 1, projectedy + 1], 5)
+                 [Px, Py], 
+                 [Px + 1, Py + 1], 5)
     pygame.display.flip()
-
 render(10, 10, 10)
 render(100, 10, 10)
 render(100, 100, 10)
@@ -34,4 +23,4 @@ render(10, 10, 100)
 render(100, 10, 100)
 render(100, 100, 100)
 render(10, 100, 100)
-    
+
